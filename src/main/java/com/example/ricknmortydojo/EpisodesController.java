3e832113491d7a0277cf53a9c6ac034a8fa13e7f
Julia Placzkowska <jplaczkowska6@gmail.com>
2022-04-20 09:42:27 +0200

    endpoint for seasons count and episodes by season

--- src/main/java/com/example/ricknmortydojo/EpisodesController.java
+++ src/main/java/com/example/ricknmortydojo/EpisodesController.java
@@ -1,17 +1,25 @@
 package com.example.ricknmortydojo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EpisodesController {
 
-    @GetMapping(value = "/joke")
-    private String getBook() {
+    @GetMapping(value = "/season/count")
+    private int getSeasonCount() {
+        return 5;
+    }
+
+    ///episodes?season=S01
+    @GetMapping(value = "/episodes")
+    private String getEpisodesBySeason(@RequestParam String season) {
         RestTemplate restTemplate = new RestTemplate();
 
-        String uri ="https://rickandmortyapi.com/api/episode";
+        String uri ="https://rickandmortyapi.com/api/episode?episode="+season;
         return restTemplate.getForObject(uri, String.class);
     }
+
 }
